f5f2dea26fee243a28f8f7289e359fa85636b1e0
Karoru4 <Karl2188@>
2023-11-25 23:59:32 +0100

    commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,12 +2,12 @@ public class Calculator {
     public static void main(String[] args) {
 
         CalculatorMethods calculatorMethods = new CalculatorMethods();
-        calculatorMethods.isEven(421);
         calculatorMethods.isEven(420);
         calculatorMethods.isOdd(421);
-        calculatorMethods.isOdd(420);
         calculatorMethods.circleFiled(13);
+        calculatorMethods.circleFiled(33);
         calculatorMethods.power(6);
+        calculatorMethods.power(634);
 
     }
 }
